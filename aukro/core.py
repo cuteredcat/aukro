e3a76ac2822049bd9f1d45a549a751af69d19391
Dmitry Minaev <dmitry.minaev@gmail.com>
2016-04-16 19:18:56 +0300

    Fix errors in seller page

--- aukro/core.py
+++ aukro/core.py
@@ -16,7 +16,7 @@ def index():
     return render_template("index.html")
 
 @core.route("/seller/")
-def index():
+def seller():
     search = request.args.get("search", "")
     seller = {}
 
@@ -25,27 +25,27 @@ def index():
 
     # try to find seller id
     data = parser.data(search)
-    if "uid" in data:
+    if data and "uid" in data:
         seller['id'] = data["uid"]
     else:
         try:
             page = parser.grab(search, tree=false)
         except:
-            error = dict(reason="Не удалось найти продавца", details="Неправильная ссылка или нет связи с сервером")
+            error = dict(reason=u"Не удалось найти продавца", details=u"Неправильная ссылка или нет связи с сервером")
             return render_template("seller.html", error=error)
 
         result = re.search("data-seller=[\"\']([0-9]+)[\"\']", page, flags=re.I | re.M)
         if result and result.group(1):
             seller['id']= result.group(1)
         else:
-            error = dict(reason="Не удалось найти продавца", details="Неправильная ссылка")
+            error = dict(reason=u"Не удалось найти продавца", details=u"Неправильная ссылка")
             return render_template("seller.html", error=error)
 
     #request seller info page
     try:
         page = parser.grab("http://aukro.ua/show_user.php?uid=%s&type=fb_seller" % seller['id'])
     except:
-        error = dict(reason="Не удалось найти информацию о продавце", details="Нет связи с сервером")
+        error = dict(reason=u"Не удалось найти информацию о продавце", details=u"Нет связи с сервером")
         return render_template("seller.html", error=error)
 
     try:
@@ -62,7 +62,7 @@ def index():
             seller['info'] = el.tostring()
 
     except:
-        error = dict(reason="Не удалось найти информацию о продавце", details="Нет нужной информации на странице")
+        error = dict(reason=u"Не удалось найти информацию о продавце", details=u"Нет нужной информации на странице")
         return render_template("seller.html", error=error)
 
     return render_template("seller.html", seller=seller)
