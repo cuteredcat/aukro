460e6484d5945b9f93b8f8e926a94f1c0504d03f
Dmitry Minaev <dmitry.minaev@gmail.com>
2016-04-16 19:12:23 +0300

    Add parser and seller page to core

--- aukro/core.py
+++ aukro/core.py
@@ -4,13 +4,65 @@
 from flask import Blueprint, jsonify, render_template, request, url_for
 
 from aukro import app, db
+from aukro.parser import Parser
+
+import re
 
 # create blueprint
 core = Blueprint("core", __name__, template_folder="templates")
 
 @core.route("/")
 def index():
-    if request.is_xhr:
-        return jsonify()
+    return render_template("index.html")
+
+@core.route("/seller/")
+def index():
+    search = request.args.get("search", "")
+    seller = {}
+
+    # init parser
+    parser = Parser(charset="utf-8")
+
+    # try to find seller id
+    data = parser.data(search)
+    if "uid" in data:
+        seller['id'] = data["uid"]
     else:
-        return render_template("index.html")
+        try:
+            page = parser.grab(search, tree=false)
+        except:
+            error = dict(reason="Не удалось найти продавца", details="Неправильная ссылка или нет связи с сервером")
+            return render_template("seller.html", error=error)
+
+        result = re.search("data-seller=[\"\']([0-9]+)[\"\']", page, flags=re.I | re.M)
+        if result and result.group(1):
+            seller['id']= result.group(1)
+        else:
+            error = dict(reason="Не удалось найти продавца", details="Неправильная ссылка")
+            return render_template("seller.html", error=error)
+
+    #request seller info page
+    try:
+        page = parser.grab("http://aukro.ua/show_user.php?uid=%s&type=fb_seller" % seller['id'])
+    except:
+        error = dict(reason="Не удалось найти информацию о продавце", details="Нет связи с сервером")
+        return render_template("seller.html", error=error)
+
+    try:
+        el = page.cssselect(".main-title .user .uname")
+        if el:
+            seller['name'] = u" ".join(el[0].xpath("./text()")).strip()
+
+        el = page.cssselect(".main-title .user .user-rating")
+        if el:
+            seller['rating'] = u" ".join(el[0].xpath("./text()")).strip()
+
+        el = page.cssselect(".feedbacksSummary")
+        if el:
+            seller['info'] = el.tostring()
+
+    except:
+        error = dict(reason="Не удалось найти информацию о продавце", details="Нет нужной информации на странице")
+        return render_template("seller.html", error=error)
+
+    return render_template("seller.html", seller=seller)
---
+++ aukro/parser.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from lxml.html import fromstring, make_links_absolute
+from urlparse import parse_qs, urlparse
+
+import cookielib, json, importlib, re, urllib, urllib2
+
+RE_URL = re.compile(r"""(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""")
+
+class HTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+    def redirect_request(self, req, fp, code, msg, headers, newurl):
+        newreq = urllib2.HTTPRedirectHandler.redirect_request(self, req, fp, code, msg, headers, newurl)
+        if newreq is not None: self.redirections.append(newreq.get_full_url())
+        return newreq
+
+class Parser():
+    def __init__(self, charset='cp1251', *args, **kwargs):
+        self.charset = charset
+        self.cookie = cookielib.CookieJar()
+
+        self.headers = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'),
+                        ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')]
+
+    def link(self, link):
+        if isinstance(link, unicode):
+            link = link.encode('utf-8')
+
+        if re.match(RE_URL, link):
+            return link
+
+        return None
+
+    def data(self, link):
+        link = self.link(link)
+
+        if link:
+            return parse_qs(urlparse(link).query, keep_blank_values=True)
+        else:
+            return None
+
+    def grab(self, link, tree=True):
+        try:
+            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
+            opener.addheaders = self.headers
+
+            link = self.link(link)
+            socket = opener.open(link)
+
+            if self.charset is None:
+                content = socket.read()
+            else:
+                content = unicode(socket.read(), self.charset)
+
+            socket.close()
+
+        except:
+            content = None
+
+        if content and tree:
+            content = make_links_absolute(fromstring(content), link, resolve_base_href=True)
+
+        return content
